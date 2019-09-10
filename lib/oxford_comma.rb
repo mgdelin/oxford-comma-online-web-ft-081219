210fb6d98b06c8d998ee86099850d67cd069a157
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-10 19:06:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
+  # this first section is saying if the array is a certain lenght do this, I chose this method because using a generalized .join etc and return was causing an error due to the fact the array didn't always have more than three strings, this allows the program to cherry pick for th conditions
   if array.length == 1
     return array[0]
   elsif array.length == 2
